78dc6d4a6bd06d84bd042de96caa8a061b731b4b
Bushra AlQahtani <89680878+bushra-alqahtani@users.noreply.github.com>
2022-05-27 18:36:48 +0300

    Create Loops.js

---
+++ Loops.js
@@ -0,0 +1,41 @@
+console.log("Print odds 1-20");
+for(var i=1;i<=20; i++) { 
+    if(i%2==0){ 
+        console.log(i); 
+        
+    } 
+} 
+console.log("Decreasing Multiples of 3");
+     
+    for(var i=100;i>=1; i--){
+        if(i%3==0){
+        console.log(i);   
+    } 
+    }
+      console.log("Print the sequence");
+     var i=4;
+    while (i>=-4){
+        console.log(i);   
+    i-=1.5;
+    }
+          console.log("Sigma");
+     
+    var sum = 0;
+      for (var i = 1; i <= 100; i++) {
+        sum += i;
+        res=sum+i;
+        console.log(res);
+      }
+      
+      console.log("Factorial");
+     
+    var product = 1;
+      for (var i = 1; i <= 12; i++) {
+       product *= i;
+        res=product;
+        console.log(product);
+      }
+     
+    
+    
+    
